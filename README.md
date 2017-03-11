8d19984d62b7a827f057de7f61e531b674e01a27
Dicky Suryadi <Dicky Suryadi>
2017-03-11 15:45:24 -0800

    Initial revision.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# dotnetify-react-demo
+#&nbsp;![alt tag](http://dotnetify.net/content/images/greendot.png) dotNetify-React Demo
+DotNetify makes it super easy to connect your React app to a cross-platform .NET back-end.  It frees you up from having to write AJAX requests or Web APIs, and your app gets real-time two-way communication through WebSockets with SignalR for free.
+
+This real-time "Hello World" demo is created with [create-react-app](https://github.com/facebookincubator/create-react-app) and runs on .NET Core back-end.
+
+Visit [http://dotnetify.net/react](http://dotnetify.net/react) for documentation and live demo.
+
+<img src="http://dotnetify.net/content/images/dotnetify-react-demo.gif">
+
+
---
+++ Startup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using DotNetify;
+
+namespace dotnetify_react_demo
+{
+   public class Startup
+   {
+      public void ConfigureServices(IServiceCollection services)
+      {
+         services.AddMemoryCache();
+         services.AddSignalR();
+         services.AddDotNetify();
+      }
+      public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+      {
+         app.UseWebSockets();
+         app.UseSignalR();
+         app.UseDotNetify();
+         app.Run(async (context) =>
+         {
+            await context.Response.WriteAsync("HelloWorld server");
+         });
+      }
+   }
+}
